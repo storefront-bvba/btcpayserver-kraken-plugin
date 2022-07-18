1870c74fddb0e22dac2cf57b63f98f94416c053c
Wouter Samaey <wouter.samaey@storefront.be>
2022-07-18 21:47:20 +0200

    Removed unneeded file

--- pack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PUBL=bin/publish
-DIST=bin/packed
-
-rm -rf $DIST/*
-dotnet publish -c Release -o $PUBL/BTCPayServer.Plugins.Custodians.Kraken
-dotnet run --project ../../BTCPayServer.PluginPacker $PUBL/BTCPayServer.Plugins.Custodians.Kraken BTCPayServer.Plugins.Custodians.Kraken $DIST
