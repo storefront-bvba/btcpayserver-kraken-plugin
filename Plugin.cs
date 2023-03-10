1772b9a63b6ac471b879415fd9f4d60d3572fa75
Wouter Samaey <wouter.samaey@storefront.be>
2023-03-10 14:14:28 +0100

    Compatibility with new plugin structure

--- Plugin.cs
+++ Plugin.cs
@@ -1,3 +1,4 @@
+using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Custodians;
 using BTCPayServer.Abstractions.Models;
 using BTCPayServer.Plugins.Custodians.Kraken.Kraken;
@@ -5,15 +6,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace BTCPayServer.Plugins.Custodians.Kraken
 {
-    public class KrakenCustodianPlugin : BaseBTCPayServerPlugin
+    
+    public class Plugin : BaseBTCPayServerPlugin
     {
-        public override string Identifier { get; } = "BTCPayServer.Plugins.Custodians.Kraken";
-        public override string Name { get; } = "Custodian: Kraken";
-        public override string Description { get; } = "Adds Kraken support for the Custodian system";
-
+        public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } = new[]
+        {
+            new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.8.2" }
+        };
+        
         public override void Execute(IServiceCollection services)
         {
-            // List all known custodians after registering CustodianRegistry... 
             services.AddSingleton<KrakenExchange>();
             services.AddSingleton<ICustodian, KrakenExchange>(provider => provider.GetRequiredService<KrakenExchange>());
         }
